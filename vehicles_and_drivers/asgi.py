e506bb56ae5e81603251f537f53627c710808fa5
yuwinzer <yu.moskalev@gmail.com>
2021-12-14 00:14:32 +0200

    start of a project, gitgnore is added

---
+++ vehicles_and_drivers/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for vehicles_and_drivers project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vehicles_and_drivers.settings')
+
+application = get_asgi_application()
