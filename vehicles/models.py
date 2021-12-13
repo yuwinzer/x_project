e3fb85ac4b624228ceaa55a08e7d7d3154773b07
yuwinzer <yu.moskalev@gmail.com>
2021-12-14 00:35:25 +0200

    added models Driver and Vehicle in apps

--- vehicles/models.py
+++ vehicles/models.py
@@ -1,6 +1,12 @@
 from django.db import models
-
+from drivers.models import Driver
 # Create your models here.
 
 class Vehicle(models.Model):
-    pass
+    id  = models.IntegerField()
+    driver_id  = models.ForeignKey(Driver, on_delete=models.SET_NULL,null=True, related_name='vehicle')
+    make  = models.CharField(max_length=64)
+    model  = models.CharField(max_length=64)
+    plate_number  = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
